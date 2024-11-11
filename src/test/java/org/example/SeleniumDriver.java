326c6be6fe87306ba9172cded44b9e180a66678e
Ianik <ianiovita005@gmail.com>
2024-11-11 16:11:04 +0200

    first commit

--- src/test/java/org/example/SeleniumDriver.java
+++ src/test/java/org/example/SeleniumDriver.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
